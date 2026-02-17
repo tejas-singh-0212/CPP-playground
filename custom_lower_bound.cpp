2d620683b6f67e2517395298bd165cb6a4b9e2df
Tejas Singh <tejassingh74823@gmail.com>
2026-02-17 13:04:08 +0530

    Creating custom lower bound function using binary search algorithm

---
+++ custom_lower_bound.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, x;
+int arr[100100];
+int check(int mid){
+    if(arr[mid] >= x) return 1;
+    return 0;    
+}
+
+int main(){
+    cin >> n;
+    for(int i=0; i < n; i++) cin >> arr[i];
+    cin >> x;
+    // lower bound using binary search
+    int low = 0, high = n-1, ans = n;
+    while (low <= high)
+    {
+        int mid = low + (high - low)/2;
+        if(check(mid) == 1){
+            ans = mid;
+            high = mid - 1;
+        }else{
+            low = mid - 1;
+        }
+    }
+    cout << ans; // index where found
+}
