f2972087fc33024210a57763ec78d9d582556288
Tejas Singh <tejassingh74823@gmail.com>
2026-05-26 14:20:20 +0530

    add: Pattern 3 of striver A2Z DSA sheet

---
+++ Patterns/3.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+1
+12
+123
+1234
+12345
+*/
+int main() {
+    int n;
+    cin >> n;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; j<=i; j++) {
+            cout << j;
+        }
+        cout << "\n";
+    }
+}
