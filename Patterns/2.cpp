e3f8df78b239ff2ea17788a86035b1c39db2f611
Tejas Singh <tejassingh74823@gmail.com>
2026-05-26 14:20:29 +0530

    add: Pattern 2 of striver A2Z DSA sheet

--- Patterns/2.cpp
+++ Patterns/2.cpp
@@ -14,10 +14,10 @@ int main() {
     int n;
     cin >> n;
     for (int i=1; i<=n; i++) {
-            for (int j=0; j<i; j++) {
-                cout << "*";
-            }
-            cout << "\n";
+        for (int j=0; j<i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
     }
     return 0;
 }
