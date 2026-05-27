8348de6ba5a10cb455d8a8d5f4c4ad41bfa1cc28
Tejas Singh <tejassingh74823@gmail.com>
2026-05-27 10:50:09 +0530

    add: Pattern 19 of striver A2Z DSA sheet

---
+++ Patterns/19.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+n=5
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+*/
+int main() {
+    int n;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n-i; j++) {
+            cout << "*";
+        } for (int j=0; j<2*i; j++) {
+            cout << " ";
+        } for (int j=0; j<n-i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (int i=1; i<=n; i++) {
+        for (int j=0; j<i; j++) {
+            cout << "*";
+        } for (int j=0; j<2*(n-i); j++) {
+            cout << " ";
+        } for (int j=0; j<i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
