0e18b9759c9001121909fe88539fbdb1dc27036f
Tejas Singh <tejassingh74823@gmail.com>
2026-06-27 23:22:05 +0530

    Time: 0 ms (100%), Space: 128.3 MB (54.33%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         vector<int> ans(n, 0);
         int pos = 0, neg = 1;
-        for(int i = 0; i<n; i++) {
+        for(int i = 0; i < n; i++) {
             if(nums[i] < 0) {
                 ans[neg] = nums[i];
                 neg += 2;
